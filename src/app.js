a38de04389c07881e6e64162bf763d7a454b9c9d
parihargaurav <parihargaurav527@gmail.com>
2026-07-01 16:22:14 +0530

    feat: implement authentication module with signup, signin, and signout functionality
    - Added user authentication routes and controllers for signup, signin, and signout.
    - Integrated JWT for token management and cookie handling for session management.
    - Implemented validation schemas using Zod for user input validation.
    - Introduced security middleware using Arcjet for rate limiting and bot detection.
    - Enhanced logging capabilities with Winston for better error tracking and monitoring.
    - Added necessary dependencies including bcrypt for password hashing and helmet for security.
    - Created utility functions for cookie management and error formatting.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,50 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+app.use(helmet()); // middleware
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => logger.info(message.trim()),
+    },
+  })
+);
+
+app.use(securityMiddleware);
+
+app.get('/health', (req, res) => {
+  res
+    .status(200)
+    .json({
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    });
 });
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+
+app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!');
+  res.status(200).send('Hello from Acquisitions!');
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
